5bbdeb15dfca38174d4761e00eb4a05538ad6cc3
Muhammad <type.92@mail.ru>
2023-02-10 23:54:16 +0300

    Seminar1DZ

---
+++ K2Sem1Dz1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 2
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+    Console.WriteLine($"Число {numberA} больше числа {numberB}");
+else if (numberA < numberB)
+    Console.WriteLine($"Число {numberA} меньше числа {numberB}");
+else
+    Console.WriteLine($"Число {numberA} равна числу {numberB}");
---
+++ K2Sem1Dz4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 8
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while(count < number)
+{
+    count++;
+    if (count%2 == 0) Console.Write($"{count} ");
+}
