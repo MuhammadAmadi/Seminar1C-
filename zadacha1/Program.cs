4a20d270d2309d11c0b7bfb7c7553927d476aa2c
Muhammad <type.92@mail.ru>
2023-02-10 21:30:47 +0300

    завершил задачу

---
+++ zadacha1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Введи число 1");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи число 2");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine($"Число {numberA} является квадратом числа {numberB}");
+}
+else
+{
+    Console.WriteLine($"Число {numberA} не является квадратом числа {numberB}");
+}
