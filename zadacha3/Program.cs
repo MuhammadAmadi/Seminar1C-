82aad852eb3d390c586ea88245f7405dd3f253bf
Muhammad <type.92@mail.ru>
2023-02-10 22:44:03 +0300

    Seminar1

---
+++ zadacha3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int minus = -number;
+
+while (minus <= number)
+{
+    Console.Write($"{minus}" );
+    minus++;
+}
---
+++ zadacha4/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите 3 значное число!");
+int value = Convert.ToInt32(Console.ReadLine());
+if (value > 99 && value < 1000)
+{
+    Console.WriteLine($"Вы ввели {value} последняя цифра {value%10}");
+}
+else
+{
+    Console.WriteLine($"Введи 3 значное число");
+}
+
